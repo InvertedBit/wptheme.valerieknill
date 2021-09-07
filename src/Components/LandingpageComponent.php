0a29f25a4a9d8506c53ce17ac8e75c80196594de
Alex Scherer <alex.scherer@outlook.com>
2021-09-07 11:09:58 +0000

    added landingpage template, component and view

---
+++ src/Components/LandingpageComponent.php
@@ -0,0 +1,17 @@
+<?php
+namespace AlexScherer\WpthemeValerieknill\Components;
+
+class LandingpageComponent extends BaseViewComponent {
+
+    public function __construct() {
+        parent::__construct('Landingpage', []);
+        $this->initialize();
+    }
+
+    
+    protected function initialize() {
+        //$this->data['images'] = $imageArray;
+    }
+
+}
+
---
+++ src/Views/Landingpage.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+    <body class="page-landingpage">
+        <div class="uk-section uk-section-secondary landingpage-section uk-light">
+            <div class="title-logo">
+                <h1 class="uk-heading-large heading-logo">Valerie Knill</h1>
+            </div>
+            <div class="link-container">
+                <div class="link-painting link" style="background-image: url('./static/img/DSC018841o.R.JPG');"><a class="link-text" href="index.html">Painting</a></div>
+                <div class="link-movies link" style="background-image: url('./static/img/header-movie01.png');"><a class="link-text" href="index-video.html">Movies</a></div>
+            </div>
+        </div>
+
+    </body>
+</html>
