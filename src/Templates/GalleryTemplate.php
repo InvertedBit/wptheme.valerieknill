c5e7af83ee278c976377ce680723125bdf39c592
Alex Scherer <alex.scherer@outlook.com>
2021-09-09 09:17:32 +0000

    added the SlimHeaderComponent with example (GalleryTemplate)

---
+++ src/Templates/GalleryTemplate.php
@@ -0,0 +1,22 @@
+<?php
+namespace AlexScherer\WpthemeValerieknill\Templates;
+
+use AlexScherer\WpthemeValerieknill\Components\FooterComponent;
+use AlexScherer\WpthemeValerieknill\Components\NavigationComponent;
+use AlexScherer\WpthemeValerieknill\Components\SlimHeaderComponent;
+
+class GalleryTemplate extends BaseTemplate {
+
+    public function __construct($parameters) {
+        parent::__construct('Gallery', $parameters);
+    }
+
+    protected function prepareComponents()
+    {
+
+        $this->addComponent(new SlimHeaderComponent());
+        $this->addComponent(new NavigationComponent(['menuLocation' => 'main-menu-painting']));
+        $this->addComponent(new FooterComponent());
+    }
+}
+
