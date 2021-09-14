bc7cd5ba55b0696f2151fa5502ee8ed7e46167d5
Alex Scherer <alex.scherer@outlook.com>
2021-09-14 14:16:04 +0000

    added Exhibitions

---
+++ src/Components/ExhibitionSliderComponent.php
@@ -0,0 +1,52 @@
+<?php
+namespace AlexScherer\WpthemeValerieknill\Components;
+
+use DateTime;
+
+class ExhibitionSliderComponent extends BaseViewComponent {
+
+    public function __construct($data = []) {
+        parent::__construct('ExhibitionSlider', $data);
+        $this->initialize();
+    }
+    
+    protected function initialize() {
+        $args = [
+            'post_type' => 'exhibition',
+            'nopaging' => true
+
+        ];
+
+        $exhibitionPosts = get_posts($args);
+        $this->data['exhibitions'] = [];
+        $this->data['exhibitions_closed'] = [];
+        $nowTime = time();
+        foreach($exhibitionPosts as $exhibitionPost) {
+            $exhibitionEndDate = $this->getField('to', $exhibitionPost->ID);
+            $exhibitionEndDatetime = DateTime::createFromFormat("d/m/Y", $exhibitionEndDate);
+            $exhibitionEndTime = $exhibitionEndDatetime->getTimestamp();
+            if ($exhibitionEndTime >= $nowTime) {
+                $this->data['exhibitions'][] = [
+                    'name' => $this->getField('name', $exhibitionPost->ID),
+                    'description' => $this->getField('description', $exhibitionPost->ID),
+                    'address' => $this->getField('address', $exhibitionPost->ID),
+                    'from' => $this->getField('from', $exhibitionPost->ID),
+                    'to' => $this->getField('to', $exhibitionPost->ID)
+                ];
+            } else {
+                $this->data['exhibitions_closed'][] = [
+                    'name' => $this->getField('name', $exhibitionPost->ID),
+                    'description' => $this->getField('description', $exhibitionPost->ID)
+                ];
+            }
+        }
+    }
+
+    public function isValid() {
+        if (empty($this->data['exhibitions'])) {
+            return false;
+        }
+        return true;
+    }
+
+}
--- src/Templates/ExhibitionsTemplate.php
+++ src/Templates/ExhibitionsTemplate.php
@@ -1,6 +1,7 @@
 <?php
 namespace AlexScherer\WpthemeValerieknill\Templates;
 
+use AlexScherer\WpthemeValerieknill\Components\ExhibitionSliderComponent;
 use AlexScherer\WpthemeValerieknill\Components\FooterComponent;
 use AlexScherer\WpthemeValerieknill\Components\NavigationComponent;
 use AlexScherer\WpthemeValerieknill\Components\SlimHeaderComponent;
@@ -18,6 +19,7 @@ class ExhibitionsTemplate extends BaseTemplate {
                 'discipline' => $this->discipline
             ]));
         $this->addComponent(new NavigationComponent(['menuLocation' => 'main-menu-painting']));
+        $this->addComponent(new ExhibitionSliderComponent());
         $this->addComponent(new FooterComponent());
     }
 }
