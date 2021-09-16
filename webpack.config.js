c7f39b7b1031a7b5a1347b6f3f6c4e4b1e1b04aa
Alex Scherer <alex.scherer@outlook.com>
2021-09-16 12:34:14 +0000

    interim commit

--- webpack.config.js
+++ webpack.config.js
@@ -25,16 +25,23 @@ module.exports = {
             },
             {
                 test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: './dist/fonts/'
-                        }
-                    }
-                ]
+                type: 'asset/resource',
+                generator: {
+                    filename: 'dist/fonts/[hash][ext][query]'
+                }
             }
+            //{
+                //test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                //use: [
+                    //{
+                        //loader: 'file-loader',
+                        //options: {
+                            //name: '[name].[ext]',
+                            //outputPath: './dist/fonts/'
+                        //}
+                    //}
+                //]
+            //}
         ]
     },
     plugins: [
