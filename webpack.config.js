475e92c6f0ee4e6e8794a57560a8b1d20ad5381b
Alex Scherer <alex.scherer@outlook.com>
2021-09-16 12:39:09 +0000

    finalized webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -30,18 +30,6 @@ module.exports = {
                     filename: 'dist/fonts/[hash][ext][query]'
                 }
             }
-            //{
-                //test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                //use: [
-                    //{
-                        //loader: 'file-loader',
-                        //options: {
-                            //name: '[name].[ext]',
-                            //outputPath: './dist/fonts/'
-                        //}
-                    //}
-                //]
-            //}
         ]
     },
     plugins: [
