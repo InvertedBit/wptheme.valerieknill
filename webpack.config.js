2018a600faadd1bfb37830600016a0a4d6756159
Alex Scherer <alex.scherer@outlook.com>
2021-09-16 11:05:14 +0000

    interim commit

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ module.exports = {
             {
                 test: /\.(sass|scss)$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './dist/fonts/'
+                        }
+                    }
+                ]
             }
         ]
     },
