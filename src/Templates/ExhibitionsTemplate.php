1c304051115ac5458b87db63a3f34736b6815cb5
Alex Scherer <alex.scherer@outlook.com>
2021-09-29 08:08:47 +0000

    removed obsolete "use" declarations

--- src/Templates/ExhibitionsTemplate.php
+++ src/Templates/ExhibitionsTemplate.php
@@ -1,14 +1,6 @@
 <?php
 namespace AlexScherer\WpthemeValerieknill\Templates;
 
-use AlexScherer\WpthemeValerieknill\Components\BreadcrumbComponent;
-use AlexScherer\WpthemeValerieknill\Components\ExhibitionSliderComponent;
-use AlexScherer\WpthemeValerieknill\Components\FooterComponent;
-use AlexScherer\WpthemeValerieknill\Components\NavigationComponent;
-use AlexScherer\WpthemeValerieknill\Components\SectionComponent;
-use AlexScherer\WpthemeValerieknill\Components\SlimHeaderComponent;
-use AlexScherer\WpthemeValerieknill\Components\TitleComponent;
-
 class ExhibitionsTemplate extends BaseTemplate {
 
     public function __construct($parameters) {
