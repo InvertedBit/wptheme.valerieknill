a1bd0d75fe3997324df603d6894ea5ca6f2beb4d
Alex Scherer <alex.scherer@outlook.com>
2021-10-11 13:33:53 +0200

    removed debug output

--- src/Views/CommentList.php
+++ src/Views/CommentList.php
@@ -19,7 +19,7 @@
                         <img class="uk-comment-avatar" src="<?php echo get_avatar_url($comment->comment_author_email); ?>" alt="" width="80" height="80">
                     </div>
                     <div class="uk-width-expand">
-                        <h4 class="uk-comment-title uk-margin-remove comment-title"><a class="uk-link-reset" href="<?php echo $comment->comment_author_url; ?>"><?php echo $comment->comment_parent . ' > ' . $comment->comment_ID . ': ' . $comment->comment_author; ?></a></h4>
+                        <h4 class="uk-comment-title uk-margin-remove comment-title"><a class="uk-link-reset" href="<?php echo $comment->comment_author_url; ?>"><?php echo $comment->comment_author; ?></a></h4>
                         <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
                             <li class="comment-date"><?php echo $comment->comment_date; ?></li>
                             <li><a href="#comments" class="reply-to-comment" data-comment-id="<?php echo $comment->comment_ID; ?>">Reply</a></li>
