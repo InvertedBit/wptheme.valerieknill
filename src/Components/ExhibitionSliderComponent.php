5948d670ddfd0c9528e3937166ab115c64d102ec
Alex Scherer <alex.scherer@outlook.com>
2021-10-18 14:49:47 +0200

    removed obsolete code

--- src/Components/ExhibitionSliderComponent.php
+++ src/Components/ExhibitionSliderComponent.php
@@ -45,23 +45,6 @@ class ExhibitionSliderComponent extends BaseIterativeViewComponent {
             
             
         } while($this->dataSource->nextItem());
-        //foreach($exhibitionPosts as $exhibitionPost) {
-            //$exhibitionEndDate = $this->getField('to', $exhibitionPost->ID);
-            //$exhibitionEndDatetime = DateTime::createFromFormat("d/m/Y", $exhibitionEndDate);
-            //$exhibitionEndTime = $exhibitionEndDatetime->getTimestamp();
-            //$exhibition = [
-                    //'name' => $this->getField('name', $exhibitionPost->ID),
-                    //'description' => $this->getField('description', $exhibitionPost->ID),
-                    //'address' => $this->getField('address', $exhibitionPost->ID),
-                    //'from' => $this->getField('from', $exhibitionPost->ID),
-                    //'to' => $this->getField('to', $exhibitionPost->ID)
-                //];
-            //if ($exhibitionEndTime >= $nowTime) {
-                //$this->data['exhibitions'][] = $exhibition;
-            //} else {
-                //$this->data['exhibitions_closed'][] = $exhibition;
-            //}
-        //}
     }
 
     public function isValid() {
